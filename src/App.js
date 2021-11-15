27cd01b89c5382a892e9928314c10d6844293125
lassebuus <lasse.buus@nettmaker.no>
2021-11-15 18:03:57 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// @src/App.jsx
 
-function App() {
+import React from "react";
+
+import styles from "./index.css";
+import Card from "./components/Card";
+import posts from "./data/posts";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-blue-100">
+      <section className="relative w-screen h-screen">
+        <div className="flex mx-auto gap-5 flex-nowrap absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">
+          {posts.map((element, index) => (
+            <Card
+              key={index}
+              title={element.title}
+              likes={element.likes}
+              order={index + 1}
+              image={element.image}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
